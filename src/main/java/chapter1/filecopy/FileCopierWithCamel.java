7fc833883a39b7a6fd72a223b23152593770651f
dennis <dennis.yuan@parcelpoint.com.au>
2020-11-01 04:55:25 +1100

    Initial commit

---
+++ src/main/java/chapter1/filecopy/FileCopierWithCamel.java
@@ -0,0 +1,22 @@
+package chapter1.filecopy;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FileCopierWithCamel {
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:data/inbox?noop=true")
+                        .to("file:data/outbox");
+            }
+        });
+        context.start();
+        // a sleep method to allow Camel application time to copy the files
+        Thread.sleep(10000);
+        context.stop();
+    }
+}
